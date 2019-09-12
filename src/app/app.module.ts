e87b33453dfcde3fc933a5d80063efdfb2682ac3
AnithaSt <anithaselvaraj37@gmail.com>
2019-09-12 21:52:05 +0530

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServiceComponent } from './service/service.component';
+import { ContactComponent } from './contact/contact.component';
+import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ServiceComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path:'',pathMatch:'full',redirectTo:'home'},
+      {path:'home',component:HomeComponent},
+      {path:'about',component:AboutComponent},
+      {path:'service',component:ServiceComponent},
+      {path:'contact',component:ContactComponent}
+      
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
